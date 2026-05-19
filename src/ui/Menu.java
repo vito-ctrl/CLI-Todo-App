c8853735c5dedd8483cd55725ff08449e5bbbd5c
vito-ctrl <aymane.elkhadraoui1@gmail.com>
2026-05-19 15:14:53 +0100

    add choise and menu method to Menu for better OOP

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -1,13 +1,23 @@
 package ui;
 import java.util.Scanner;
 public class Menu {
-    public void start(){
-        Scanner scan = new Scanner(System.in);
+
+    private void menu(){
         System.out.println("welcom to the To do App CLI");
         System.out.println("1 . add task ");
         System.out.println("2 . delete task ");
         System.out.println("3 . list tasks ");
         System.out.println("4 . mark completed ");
+    }
+
+    private int choise () {
+        Scanner scan = new Scanner(System.in);
         System.out.print(":: ");
+        int choise = scan.nextInt();
+        return choise;
+    }
+    public void start(){
+        menu();
+        choise();
     }
 }
